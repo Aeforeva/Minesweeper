1ace9045ab7310088a52e3080f4a25105dbf0373
Aeforeva <83841383+Aeforeva@users.noreply.github.com>
2022-12-14 14:26:15 +0300

    Init

---
+++ app/src/main/java/com/example/minesweeper/ui/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.minesweeper.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/example/minesweeper/ui/OneFragment.kt
@@ -0,0 +1,27 @@
+package com.example.minesweeper.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.minesweeper.R
+
+class OneFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+}
